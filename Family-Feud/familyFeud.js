38b72d67cc1545eee490738fd95c0da20758b0c9
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2025-04-12 01:30:11 +0300

    FamilyFeud game

---
+++ Family-Feud/familyFeud.js
@@ -0,0 +1,85 @@
+var questions = [];
+var questionChart;
+
+$(document).ready(function(){
+    $("#csv-file").change(handleFileSelect);
+    //loadChart();
+});
+
+function handleFileSelect(evt) {
+    //Makes element visible
+    document.getElementById("questionsContainer").style.display = "block";
+
+    //handle File Select
+    var file = evt.target.files[0];
+
+    Papa.parse(file, { 
+        complete: function(results) {
+            questions = results["data"];
+            console.log(questions);
+        }
+    });
+}
+
+function loadQuestion() {
+    var randomIndex = Math.floor((Math.random() * questions.length) + 1);
+    loadChart(questions[randomIndex]);
+    showAnswers(questions[randomIndex]);
+}
+
+function loadChart(question) {
+    var context = document.getElementById('myChart').getContext('2d');
+    if(typeof questionChart !== 'undefined') {
+        questionChart.destroy();
+    }
+
+    var title = question[0];
+    var answers = question.filter(function(element, index, array) {
+        return (index % 2 === 1);
+    });
+    var percentages = question.filter(function(element, index, array) {
+        return (index % 2 === 0);
+    });
+    percentages.shift();
+
+    questionChart = new Chart(context, {
+        type: 'bar',
+        data: {
+            labels: answers,
+            datasets: [{
+                label: title,
+                data: percentages,
+                backgroundColor: 'rgb(75, 99, 132, 0.2)',
+                borderColor: 'rgb(75, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+}
+
+function showAnswers(question) {
+    var title = question[0];
+    var answers = question.filter(function(element, index, array) {
+        return (index % 2 === 1);
+    });
+    var percentages = question.filter(function(element, index, array) {
+        return (index % 2 === 0);
+    });
+    percentages.shift();
+
+    var orderedList = $("#shownAnswers");
+    orderedList.empty();
+    for(var i = 0; i< answers.length; ++i) {
+        orderedList.append( '<li>' + (i+1) + ' ' + percentages[i] + ' '+ answers[i] + '</li>' );
+    }
+}
---
+++ Family-Feud/readme.txt
@@ -0,0 +1,10 @@
+1. download the folder somewhere
+2. open familyFeud.html in your favorite browser
+3. ???
+4. profit (mostly spiritual)
+
+note1: the questions files are in the questions/ directory
+note2: the naming of the files in the questions/ directory refers to, perhaps obviously, the number of answers the questions in the file have
+note3: the Questions.csv file has all the other files questions (except for FastMoney.csv) concatenated
+note4: the exact percentage of the answers is shown on the right end of the tooltips (after the question)
+note5: something I forget but it's late
